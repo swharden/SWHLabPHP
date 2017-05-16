a4b6854bfdc462b0f88799bf22005352f277c87f
Scott W Harden <swharden@gmail.com>
2017-05-15 20:57:47 -0400

    improved menu

--- src/templates/01_barebones/page_menu.php
+++ src/templates/01_barebones/page_menu.php
@@ -16,6 +16,7 @@ function colorcode_lookup($s){
 function project_displayItems($items){
     // after getting a list of items from project_getItems(), this function
     // turns the list into beautifully formatted HTML.
+    global $project;
     foreach ($items as $line){
         list($cellID,$color,$desc)=$line;
         $color=colorcode_lookup($color);
@@ -24,13 +25,14 @@ function project_displayItems($items){
             echo "<div class='menu_category'>$desc</div>";
         } else {
             // this is a single cell
-            echo "<div class='menu_cell_ID' style='background-color: $color'>$cellID";
+            echo "<div class='menu_cell_ID' style='background-color: $color'>";
+            echo "<a href='?page=cellID&project=$project&cellID=$cellID' target='content'>$cellID</a>";
             echo "<span class='menu_cell_description'>$desc</span></div>";
         }
     }
 }
 
-$items=project_getItems("\\\\Spike\\X_Drive\\Data\\SCOTT\\2017-04-24 aging BLA");
+$items=project_getItems($project);
 project_displayItems($items);
 
 /*
