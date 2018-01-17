99bfc2981dd88f99bae60c2b372147859c2e703a
Scott W Harden <swharden@gmail.com>
2018-01-17 14:35:11 -0500

    config file now contains python path

--- recode/src/config/config.php
+++ recode/src/config/config.php
@@ -4,10 +4,12 @@ $__PATH_DATA_LOCAL__=realpath("D:\\X_Drive\\Data"); // data folder (local path)
 $__PATH_DATA_NETWORK__=realpath("X:\\Data"); // data folder (network path)
 $__PATH_DATA_WEB__="/dataX"; // what it gets remapped to
 $__PATH_SRC_WEB__="/SWHLabPHP/recode/src"; // web-accessable path to folder containing /src/index.php
+$__PATH_PYTHON__='C:\ProgramData\Anaconda3\python.exe'; // path to python.exe
 
 $GLOBALS['__PATH_DATA_LOCAL__'] = $__PATH_DATA_LOCAL__;
 $GLOBALS['__PATH_DATA_NETWORK__'] = $__PATH_DATA_NETWORK__;
 $GLOBALS['__PATH_DATA_WEB__'] = $__PATH_DATA_WEB__;
 $GLOBALS['__PATH_SRC_WEB__'] = $__PATH_SRC_WEB__;
+$GLOBALS['__PATH_PYTHON__'] = $__PATH_PYTHON__;
 
 ?>
